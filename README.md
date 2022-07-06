10bc7192087f32944b4e3128ca441b496cb59bb3
unknown <cbf252@gmail.com>
2022-07-06 17:07:40 +0300

    start sysprep

--- README.md
+++ README.md
@@ -133,3 +133,8 @@ For adding permissions to mailboxes folders like calender, contacts, etc. betwee
 the script is written in Python. In Jupiter Notebook
 you enter path of csv file with the format specified in the example file.
 and export the powershell script to the text file.
+
+## Sysprep automation
+
+Sometime Sysprep fails after you remove or update Microsoft Store apps that include built-in Windows images
+the script try to run sysprep if fails it will open the Sysprep log check for errors ask the user to sellect the error line (just for the first time)
---
+++ Sysprep automation/sysperp automation.py
@@ -0,0 +1,52 @@
+import os
+
+
+def try_prep():
+    '''This function try to run sysprep(generalize)and shutdown the system'''
+    os.system('C:\Windows\System32\Sysprep\Sysprep.exe /generalize /shutdown /oobe')
+
+
+def get_lines_from_log_file():
+    file_path = 'C:\Windows\System32\Sysprep\Panther\setupact.log'
+    lines = open(file_path).readlines()
+    return lines
+
+def get_erorr_line_number(lines):
+    last_try = lines[-30:]
+    l = 1
+    for line in last_try:
+        print('[',l,']',line)
+        l = l+ 1
+    error_line = int(input('>> please enter the first erorr line number: '))
+    line = last_try[error_line-1]
+    print('\n>> selected line is ',line)
+    return error_line-1
+
+def get_error_line(lines,line_number):
+    last_try = lines[-30:]
+    line_number = abs(line_number)
+    return last_try[line_number]
+
+def pars_line(line):
+    slpited_line = line.split()
+    if slpited_line[4] == 'Package':
+        pkg_name = slpited_line[5]
+        print('>> package name')
+        print(pkg_name)
+        return pkg_name
+    else:
+        print('selected line not correct or it is not packege problem check log file')
+        exit()
+
+def remove_pkg(pkg_name):
+    os.system('powershell.exe Remove-AppxPackage -allusers -Package "'+pkg_name+'"')
+    print('\n'+pkg_name+' removed')
+
+try_prep()
+line_number = get_erorr_line_number(get_lines_from_log_file())
+
+for i in range(2):
+    try_prep()
+    pkg_name = pars_line(get_error_line(get_lines_from_log_file(),line_number))
+    remove_pkg(pkg_name)
+
